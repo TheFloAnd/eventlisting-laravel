2036d94417994288bfeafaf94a3ebe6fbe060105
TheFloAnd <holst568@gmail.com>
2022-01-17 09:37:08 +0100

    first commit

---
+++ database/migrations/2021_11_21_092008_create_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->smallInteger('not_applicable')->nullable();
+            $table->string('event');
+            $table->datetime('start_at');
+            $table->datetime('end_at');
+            $table->integer('repeat')->nullable();
+            $table->integer('repeat_parent')->nullable();
+            $table->string('room')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
---
+++ database/seeders/SettingsSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SettingsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $settings = array([
+            'view_name' => 'Automatisches Neuladen',
+            'setting' => 'refresh',
+            'value' => '15',
+            'value_unit' => 'seconds',
+            'created_at' => strftime('%Y-%m-%d %H:%M:%S')
+        ],
+        [
+            'view_name' => 'Termin Preview Zeitraum',
+            'setting' => 'future_day',
+            'value' => '2',
+            'value_unit' => 'week',
+            'created_at' => strftime('%Y-%m-%d %H:%M:%S')
+        ],
+        [
+            'view_name' => 'Überschrift',
+            'setting' => 'name',
+            'value' => 'Unset',
+            'created_at' => strftime('%Y-%m-%d %H:%M:%S')
+        ]);
+
+        foreach ($settings as $setting){
+            DB::table('settings')->insert($setting);
+        }
+
+    }
+}
