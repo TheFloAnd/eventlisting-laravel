081bc5fdaba4b711de12e71452d73bbcf5d02d43
thefloand <holst568@gmail.com>
2022-03-14 12:20:09 +0000

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<link rel="icon" href="{{ asset('img/svg/calendar-event-fill.svg') }}">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="icon" href="{{ asset('img/svg/calendar-event-fill.svg') }}">
     <title>{{ $title->value ?? 'Termine' }}</title>
 
     <!-- Fonts -->
