aae892fa9eb66f77b5398e96f0dd90c255f9598b
thefloand <holst568@gmail.com>
2022-03-22 07:11:03 +0000

    routing + permission

--- resources/views/components/home_nav.blade.php
+++ resources/views/components/home_nav.blade.php
@@ -25,6 +25,8 @@
             </a>
         </div>
         <div class="btn-group-vertical w-100" role="group">
+            @if(Route::has('events'))
+            @can('events')
             <div class="btn-group-vertical w-100" role="group">
                 <a href="{{ route('events') }}" type="button"
                     class="btn btn-outline-secondary home-offcanvas-body-link">
@@ -35,6 +37,8 @@
                         </svg></span>
                     {{ __('Termine') }}
                 </a>
+                @if(Route::has('events.create'))
+                @can('events-create')
                 <li class="home-offcanvas-body-second-link">
                     <a href="{{ route('events.create') }}" type="button"
                         class="btn btn-outline-secondary home-offcanvas-body-link home-offcanvas-body-second-link">
@@ -46,7 +50,13 @@
                         {{ __('Hinzufügen') }}
                     </a>
                 </li>
+                @endcan
+                @endif
             </div>
+            @endcan
+            @endif
+            @if(Route::has('groups'))
+            @can('groups')
             <div class="btn-group-vertical w-100" role="group">
                 <a href="{{ route('groups') }}" type="button"
                     class="btn btn-outline-secondary home-offcanvas-body-link"><span class="btn-label"><svg
@@ -57,6 +67,8 @@
                         </svg></span>
                     {{ __('Gruppen') }}
                 </a>
+                @if(Route::has('groups.create'))
+                @can('groups-create')
                 <li class="home-offcanvas-body-second-link">
                     <a href="{{ route('groups.create') }}" type="button"
                         class="btn btn-outline-secondary home-offcanvas-body-link home-offcanvas-body-second-link">
@@ -68,7 +80,12 @@
                         {{ __('Hinzufügen') }}
                     </a>
                 </li>
+                @endcan
+                @endif
             </div>
+            @endcan
+            @endif
+            @if(Route::has('settings'))
             @can('settings')
             <a href="{{ route('settings') }}" type="button" class="btn btn-outline-secondary home-offcanvas-body-link">
                 <span class="btn-label">
@@ -83,11 +100,14 @@
                 {{ __('Einstellungen') }}
             </a>
             @endcan
-@can('database')
-<a href="{{ route('database') }}" type="button" class="btn btn-outline-secondary home-offcanvas-body-link">
+
+            @endif
+            @if(Route::has('database'))
+            @can('database')
+            <a href="{{ route('database') }}" type="button" class="btn btn-outline-secondary home-offcanvas-body-link">
                 <span class="btn-label">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive"
-                        viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-archive" viewBox="0 0 16 16">
                         <path
                             d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z" />
                     </svg>
@@ -95,6 +115,39 @@
                 {{ __('Datenbank') }}
             </a>
             @endcan
+            @endif
+            @if(Route::has('users'))
+            @can('user-list')
+            <a href="{{ route('users') }}" type="button" class="btn btn-outline-secondary home-offcanvas-body-link">
+                <span class="btn-label pe-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-gear" viewBox="0 0 16 16">
+                        <path
+                            d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
+                        <path
+                            d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
+                    </svg>
+                </span>
+                {{ __('Benutzer') }}
+            </a>
+            @endcan
+            @endif
+            {{-- @if(Route::has('roles'))
+            @can('role-list')
+            <a href="{{ route('roles') }}" type="button" class="btn btn-outline-secondary home-offcanvas-body-link">
+                <span class="btn-label pe-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-gear" viewBox="0 0 16 16">
+                        <path
+                            d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
+                        <path
+                            d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
+                    </svg>
+                </span>
+                {{ __('Rolen') }}
+            </a>
+            @endcan
+            @endif --}}
         </div>
         <div class="mt-3">
             <a type="button" class="btn btn-outline-danger w-100" href="{{ route('logout') }}"
